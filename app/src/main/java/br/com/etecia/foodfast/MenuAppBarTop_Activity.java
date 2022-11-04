14faa587e79cdc5f945b37e5c7e10c90c0b192d5
Dodoh04 <douglasoliversa3@gmail.com>
2022-11-03 21:08:12 -0300

    declarando a variável global do objeto AppBar

--- app/src/main/java/br/com/etecia/foodfast/MenuAppBarTop_Activity.java
+++ app/src/main/java/br/com/etecia/foodfast/MenuAppBarTop_Activity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MenuAppBarTop_Activity extends AppCompatActivity {
+    //declarando a variável global do objeto AppBar
+    MaterialToolbar idToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_app_bar_top_layout);
+        //apresentando a toolbar xml para o java
+        idToolbar = findViewById(R.id.idToolbar);
     }
 }
